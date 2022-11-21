9ec202b4f154898f07a52c45fb602adb19a138ab
user <user@DESKTOP-U59IHVT>
2022-11-21 11:09:23 +0900

    인환 파일 만듬

--- gittest/src/main/java/gittest/inhwan.java
+++ gittest/src/main/java/gittest/inhwan.java
@@ -1,13 +1,10 @@
 package gittest;
 
-public class test {
+public class inhwan {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		
-		System.out.println("팀장 커밋!!");
-		
+		System.out.println("인환");
 	}
 
 }
