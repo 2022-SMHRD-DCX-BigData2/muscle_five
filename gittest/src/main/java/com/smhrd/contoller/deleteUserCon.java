c7afeb82041d76c8a0e0773d7153b7f33fcb0739
user <user@DESKTOP-U59IHVT>
2022-11-25 17:29:49 +0900

    11

--- gittest/src/main/java/com/smhrd/contoller/deleteUserCon.java
+++ gittest/src/main/java/com/smhrd/contoller/deleteUserCon.java
@@ -25,9 +25,11 @@ public class deleteUserCon extends HttpServlet {
 		HttpSession session = request.getSession();
 		Member loginMember = (Member) session.getAttribute("loginMember");
 		String id = loginMember.getId();
+		
 		String pw = request.getParameter("pw");
 		System.out.println(id);
 		System.out.println(pw);
+		
 		// DAO에 일할 메소드 만들기
 		Member deletemember = new Member(id, pw);
 		MemberDAO dao = new MemberDAO();
